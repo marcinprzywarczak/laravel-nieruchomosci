51c7ed3f1155dc62f4d59f7d73f407232be77c00
marcin <marcin.przywarczak@gmail.com>
2021-11-06 10:11:31 +0100

    Dodanie seederów ról i uprawnień

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Database\Seeders\Auth\UsersSeeder;
 use Illuminate\Database\Seeder;
+use Database\Seeders\Auth\RolesSeeder;
+use Database\Seeders\Auth\UsersSeeder;
+use Database\Seeders\Auth\PermissionsSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(RolesSeeder::class);
+        $this->call(PermissionsSeeder::class);
         $this->call(UsersSeeder::class);
     }
 }

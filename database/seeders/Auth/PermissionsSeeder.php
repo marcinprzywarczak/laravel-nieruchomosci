fa82e73779cbb34780d04e41f9f32ae3cb7edbb0
marcin <marcin.przywarczak@gmail.com>
2021-11-06 10:11:54 +0100

    Definicja seedera uprawnień

---
+++ database/seeders/Auth/PermissionsSeeder.php
@@ -0,0 +1,25 @@
+<?php
+namespace Database\Seeders\Auth;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\PermissionRegistrar;
+
+class PermissionsSeeder extends Seeder
+{
+    public function run()
+    {
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        Permission::create(['name' => 'users.index']);
+        Permission::create(['name' => 'users.store']);
+        Permission::create(['name' => 'users.destroy']);
+        Permission::create(['name' => 'users.change_role']);
+
+        $userRole = Role::findByName(config('app.admin_role'));
+        $userRole->givePermissionTo('users.index');
+        $userRole->givePermissionTo('users.store');
+        $userRole->givePermissionTo('users.destroy');
+        $userRole->givePermissionTo('users.change_role');
+    }
+}

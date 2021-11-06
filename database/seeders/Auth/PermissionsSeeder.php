f1ab1905b94c1abebcc0f5d52e9673723ed1d207
marcin <marcin.przywarczak@gmail.com>
2021-11-06 11:58:11 +0100

    Dodanie uprawnienia przeglądania logów. Nadanie go adminowi

--- database/seeders/Auth/PermissionsSeeder.php
+++ database/seeders/Auth/PermissionsSeeder.php
@@ -10,13 +10,14 @@ class PermissionsSeeder extends Seeder
     public function run()
     {
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
-
+        Permission::create(['name' => 'log-viewer']);
         Permission::create(['name' => 'users.index']);
         Permission::create(['name' => 'users.store']);
         Permission::create(['name' => 'users.destroy']);
         Permission::create(['name' => 'users.change_role']);
 
         $userRole = Role::findByName(config('app.admin_role'));
+        $userRole->givePermissionTo('log-viewer');
         $userRole->givePermissionTo('users.index');
         $userRole->givePermissionTo('users.store');
         $userRole->givePermissionTo('users.destroy');

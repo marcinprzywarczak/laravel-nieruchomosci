96e0a71a33890a39e38704eddb4c80b5d25c85b7
marcin <marcin.przywarczak@gmail.com>
2021-11-06 10:12:46 +0100

    Dodanie nowego użytkownika. Przypisanie im ról

--- database/seeders/Auth/UsersSeeder.php
+++ database/seeders/Auth/UsersSeeder.php
@@ -5,13 +5,29 @@ namespace Database\Seeders\Auth;
 use Carbon\Carbon;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Hash;
 
 class UsersSeeder extends Seeder
 {
     public function run()
     {
-        User::create(
+
+        $user = User::create(
+            [
+                'name' => 'Admin Testowy',
+                'email' => 'admin.testowy@localhost',
+                'email_verified_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'password' => Hash::make('12345678')
+            ]
+            );
+        $adminRole = Role::findByName(config('app.admin_role'));
+        if(isset($adminRole))
+        {
+            $user->assignRole($adminRole);
+        }
+
+        $user = User::create(
             [
                 'name' => 'User Testowy',
                 'email' => 'user.testowy@localhost',
@@ -19,5 +35,10 @@ class UsersSeeder extends Seeder
                 'password' => Hash::make('12345678')
             ]
             );
+        $userRole = Role::findByName(config('app.user_role'));
+        if(isset($userRole))
+        {
+            $user->assignRole($userRole);
+        }
     }
 }

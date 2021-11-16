83233e0e7172ce999e82b6a53ecf52405e1f6d27
marcin <marcin.przywarczak@gmail.com>
2021-11-16 13:02:30 +0100

    Wywołanie seederow nieruchomosci, ofert, typow i statusow

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,12 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Seeders\OfferSeeder;
+use Database\Seeders\PropertySeeder;
 use Database\Seeders\Auth\RolesSeeder;
 use Database\Seeders\Auth\UsersSeeder;
+use Database\Seeders\Offer_statusSeeder;
+use Database\Seeders\Property_typeSeeder;
 use Database\Seeders\Auth\PermissionsSeeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,5 +23,10 @@ class DatabaseSeeder extends Seeder
         $this->call(RolesSeeder::class);
         $this->call(PermissionsSeeder::class);
         $this->call(UsersSeeder::class);
+
+        $this->call(Property_typeSeeder::class);
+        $this->call(Offer_statusSeeder::class);
+        $this->call(PropertySeeder::class);
+        $this->call(OfferSeeder::class);
     }
 }

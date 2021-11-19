f1eac03224e9d4286ffc9bedd396768d2dddb834
marcin <marcin.przywarczak@gmail.com>
2021-11-19 17:26:51 +0100

    dodanie tłumaczeń typów nieruchomości

--- resources/lang/pl/translations.php
+++ resources/lang/pl/translations.php
@@ -4,5 +4,21 @@ return[
     'menu' =>[
         'log-viewer' => 'Logi'
     ],
-    'labels' => []
+    'attribute' =>
+    [
+        'created_at' => 'utworzono',
+        'updated_at' => 'zaktualizowano',
+        'deleted_at' => 'usunięto'
+    ],
+    'labels' => [],
+    'property_types' => 
+    [
+        'title' => 'Typy nieruchomości',
+        'attribute' =>
+        [
+            'name' => 'nazwa',
+            'address' => 'adres',
+            'count_properties' => 'ilość nieruchomości'
+        ]
+    ],
 ];

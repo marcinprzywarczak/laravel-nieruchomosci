bfcca5422f31fa576ee34f333dd78ad43a67d28f
marcin <marcin.przywarczak@gmail.com>
2021-11-19 19:05:55 +0100

    Dodanie skryptu i css w celu uruchomienia wtyczki datatables (paginacja po stronie klienta)

---
+++ resources/js/offer_statuses.js
@@ -0,0 +1,14 @@
+require('datatables.net-bs5');
+
+$(function()
+{
+    $('table').DataTable(
+        {
+            "language" : 
+            {
+                "url" : "vendor/datatables/i18n/" + config.locale + ".json"
+            }
+        }
+        
+    );
+});

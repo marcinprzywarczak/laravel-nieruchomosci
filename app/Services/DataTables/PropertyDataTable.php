b9a6621c216cc1fc34ed36ac0821ff322bc591e0
marcin <marcin.przywarczak@gmail.com>
2021-11-25 18:36:48 +0100

    Serwis data table nieruchomości do zapytań asynchronicznych

---
+++ app/Services/DataTables/PropertyDataTable.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services\DataTables;
+
+use App\Models\Property;
+use Yajra\DataTables\Services\DataTable;
+
+class PropertyDataTable extend DataTable
+{
+    public function ajax()
+    {
+        $datatable = DataTables::eloquen($this->query())
+            ->editColumn('created_at', function($row)
+            {
+                return $row->created_at
+                    ?with(new Carbon($row->created_at))->format('Y-m-d')
+                    : '';
+            })
+            ->editColumn('updated_at', function($row)
+            {
+                return $row->updated_at
+                    ?with(new Carbon($row->updated_at))->format('Y-m-d H:i')
+                    : '';
+            })
+            ->editColumn('deleted_at', function($row)
+            {
+                return $row->deleted_at
+                    ?with(new Carbon($row->deleted_at))->format('d-m-Y')
+                    : '';
+            })
+            ->addColumn('action', function($row)
+            {
+                return '';
+            })
+            ->rawColumns(['action']);
+
+        return $datatable->make(true);
+    }
+
+    public function query()
+    {
+        $rows = Property::withTrashed();
+        return $this->applyScopes($rows);
+    }
+}

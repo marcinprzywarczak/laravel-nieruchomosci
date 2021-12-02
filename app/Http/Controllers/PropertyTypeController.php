3d744c71628540e2848a035a07d0dda50dd6f7f0
marcin <marcin.przywarczak@gmail.com>
2021-12-02 17:00:54 +0100

    Metody do dodawania typów nieruchomości

--- app/Http/Controllers/PropertyTypeController.php
+++ app/Http/Controllers/PropertyTypeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\PropertyType;
 use Illuminate\Http\Request;
+use App\Http\Requests\PropertyTypes\PropertyTypeRequest;
 
 class PropertyTypeController extends Controller
 {
@@ -16,5 +17,20 @@ class PropertyTypeController extends Controller
                     ->withCount('properties')->get()
             ]
             );
+    }
+    public function create()
+    {
+        return view('property_types.create');
+    }
+
+    public function store(PropertyTypeRequest $request)
+    {
+        $property_type = PropertyType::create(
+            $request->all()
+        );      
+        return redirect()->route('property_types.index')
+            ->with('success', __('translations.property_types.flashes.success.stored', [
+                'name' => $property_type->name
+            ]));
     }
 }

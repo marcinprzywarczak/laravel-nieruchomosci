6faf8092e018acd29ffce5e5551c87d307151a1e
marcin <marcin.przywarczak@gmail.com>
2021-12-29 14:30:52 +0100

    Zmiana reguły walidacji, wtyczka js do walidacji po stronie klienta nie obługuje tej reguły

--- app/Http/Requests/Offers/OfferRequest.php
+++ app/Http/Requests/Offers/OfferRequest.php
@@ -28,7 +28,7 @@ class OfferRequest extends FormRequest
             'offer_status_id' => ['required', 'integer', 'exists:offer_statuses,id'],
             'title' => ['required', 'string', 'max:100'],
             'start_date' => ['required','date'],
-            'end_date' => ['required','date', 'after:start_date'],
+            'end_date' => ['required','date'],
             'price' => ['required',  'numeric', 'min:1'],
             'comment' => ['nullable', 'string', 'max:255'],
         ];
